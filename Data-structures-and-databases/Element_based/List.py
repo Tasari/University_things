c606b522c6072ae748c422eb7af03b8d78aa1058
Tasari <Tasari@wp.pl>
2020-03-20 19:37:04 +0100

    Finished List

--- Data-structures-and-databases/Element_based/List.py
+++ Data-structures-and-databases/Element_based/List.py
@@ -1,4 +1,4 @@
-from Element import Element
+from Element import Element, Key
 
 class ElementList(Element): 
     def __init__(self, s = 97, h = 53, data = None, right = None): 
@@ -14,9 +14,46 @@ class ElementList(Element):
         assert nextval is None or isinstance(nextval,ElementList), "Error"
         self.__right = nextval
 
-a = ElementList()
-b = ElementList()
-a.right = b
-print(a)
-print(b)
-print(a.right is b)
+class List:
+    def __init__(self):
+        self.__head = None
+
+    def insert(self, e):
+        e.right = self.__head
+        self.__head = e
+    
+    def search(self, e):
+        x = self.__head
+        while x is not None and x.key != e:
+            x = x.right
+        return x
+    
+    def delete(self, x):
+        y = self.__head
+        if x is y:
+            self.__head = x.right
+        while y is not None and y.right is not x:
+            y = y.right
+        try:
+            y.right = x.right
+            x.next = None
+        except:
+            pass
+
+    def __repr__(self):
+        x = self.__head
+        s = ''
+        while x is not None:
+            s += str(x)
+            x = x.right
+        return s
+
+
+l = List()
+l.insert(ElementList(data=chr(14)))
+l.insert(ElementList(25))
+l.insert(ElementList(data=chr(34)))
+print(l)
+x = l.search(Key(34))
+l.delete(x)
+print(l)
